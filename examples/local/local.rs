81bf6617d9f2847e6e31a2ff51ea4a728b5591e5
Cole Mackenzie <colemackenzie1@gmail.com>
2024-12-29 17:58:00 -0800

    Use hyper::Server instead of axum::Server

--- examples/local/local.rs
+++ examples/local/local.rs
@@ -63,7 +63,7 @@ async fn main() {
         .with_state(state);
 
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    hyper::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
